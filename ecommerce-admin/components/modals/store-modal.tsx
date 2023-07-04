06f5dddf63d03c5cef8b9d98037a79f138ee74bd
BanuDanabakiyam <banubala9655@gmail.com>
2023-07-04 08:47:18 +0530

    done modalComponent

--- ecommerce-admin/components/modals/store-modal.tsx
+++ ecommerce-admin/components/modals/store-modal.tsx
@@ -9,7 +9,7 @@ export const StoreModal = () => {
         <Modal
         title="Create Store"
         description="Add a new store to manage products and categories "
-        isOpen={false}
+        isOpen={true}
         onClose={() => {}}
         >
             Future Create Store Form
--- ecommerce-admin/providers/modal-provider.tsx
+++ ecommerce-admin/providers/modal-provider.tsx
@@ -1,6 +1,7 @@
 "use client";
+import { useEffect, useState } from "react"; // Global import
 
-import { useEffect, useState } from "react";
+import { StoreModal } from "@/components/modals/store-modal";
 
 export const ModalProvider = () => {
     const [isMounted, setIsMounted] = useState(false);
@@ -8,4 +9,33 @@ export const ModalProvider = () => {
     useEffect(() => {
         setIsMounted(true);
     },[]);
+
+    if( !isMounted ){
+        return null;
+    }
+    return (
+        <>
+        <StoreModal />
+        </>
+    );
 }
+
+
+//  ********************************************
+//  what is hydration error
+// "undefined" caused the hydration error, which occurs if the server-rendered HTML does not match the client-rendered HTML.
+// eg:
+// <LinK>
+// <a>
+//     <Image>
+//         ........
+//     </Image>
+// </a>
+// </LinK> 
+
+// In this example, you will get the React Hydration Error if the <a> tag is used inside another <a> tag. because in Link tag alreday has <a> hidden tag.
+// we ensure until useEffect lifecycle run something happen in client component to avoid hydration error.
+// ***********************************************
+
+
+

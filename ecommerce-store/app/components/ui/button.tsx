7cbb737937cec9a4c28b9e9465fb8a1743f8afdf
BanuDanabakiyam <banubala9655@gmail.com>
2023-07-21 00:23:52 +0530

    products

---
+++ ecommerce-store/app/components/ui/button.tsx
@@ -0,0 +1,46 @@
+//  Own button component
+
+import { forwardRef } from "react";
+import { cn } from "@/lib/utils";
+
+export interface ButtonProps
+ extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
+
+ const Button = forwardRef<HTMLButtonElement, ButtonProps>(({
+    className,
+    children,
+    disabled,
+    type = 'button',
+    ...props
+   //  The second argument is the ref received from the parent component.
+ }, ref) => {
+    return(
+        <button 
+        className={cn(
+         `
+         w-auto
+         rounded-full
+         bg-black
+         border-transparent
+         px-5
+         py-3
+         disabled: cursor-not-allowed
+         disabled:opacity-50
+         text-white
+         font-semibold
+         hover:opacity-75
+         transition
+         `, className 
+         // classname This is a prop passed to the 
+         // component from the parent.
+        )}
+        ref={ref}>
+         {children}
+         {/* React represent content of the button */}
+        </button>
+    );
+ })
+
+ Button.displayName = "Button";
+
+ export default Button;
--- ecommerce-store/types.ts
+++ ecommerce-store/types.ts
@@ -1,6 +1,6 @@
 export interface Billboard {
     id: string;
-    name: string;
+    label: string;
     imageUrl: string;
 };
 
@@ -9,3 +9,48 @@ export interface Category {
     name: string;
     billboard: Billboard;
 }
+
+export interface Product {
+    id: string;
+    category: Category;
+    name: string;
+    price: string;
+    isFeatured: boolean;
+    size: Size;
+    color: Color;
+    images: Image[];
+};
+
+export interface Image {
+    id: string;
+    url: string;
+};
+
+export interface Size {
+    id: string;
+    name: string;
+    value: String;
+};
+
+export interface Color {
+    id: string;
+    name: string;
+    value: String;
+}
+
+
+// In summary, the Billboard interface specifies
+//  that an object adhering to this interface 
+//  should have three properties: id, name, and
+//   imageUrl, each with its own specific data type. 
+//   By using this interface, you can define objects 
+//   that represent billboards in your TypeScript code,
+//    ensuring that they contain the required properties 
+//    with the correct data types. This provides type safety
+//     and helps catch potential errors during development, as 
+//     TypeScript will check that objects conform to the interface
+//      when they are assigned to variables or used as function parameters.
+
+
+
+
